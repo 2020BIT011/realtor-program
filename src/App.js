0ea3bbe518ca8fdf473589b7cc7fefaf36a8a13e
Vishwajit <2020bit011@sggs.ac.in>
2023-04-11 08:38:29 +0530

    create listing page and use swiper to add  the images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-// import CreateListing from "./pages/CreateListing";
+import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -26,8 +28,14 @@ function App() {
 </Route>
 < Route path="/sign-in" element={<SignIn />}/>
 < Route path="/sign-up" element={<SignUp />}/>
-< Route path="/offers" element={<Offers />}/>
 < Route path="/forgot-Password" element={<ForgotPassword />}/>
+<Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+< Route path="/offers" element={<Offers />}/>
+<Route path="/category/:categoryName" element={<Category />} />
+        
 < Route path="create-listing" element={<PrivateRoute />}>
 < Route path="/create-listing" element={<CreateListing />}/>
 </Route>
@@ -43,24 +51,19 @@ function App() {
 </Router>
 
 <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/>
-{/* Same as */}
-<ToastContainer />
-
-
-</>
-
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 }
 
-export default App;
+export default App
